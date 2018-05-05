e435c1cfb310c90a87785f4fc6bb08e979aeb678
Jimmyqu <laojie420@163.com>
2018-05-05 17:00:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!--serach component-->
+    <home-header></home-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from './components/homeheader'
+export default {
+  name: 'App',
+  components:{
+    HomeHeader
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/homeheader.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <mt-search
+        v-model="value"
+      >
+        <mt-cell
+          v-for="item in list"
+          :title="item.year"
+          :value="item.title"
+          @click.native="itemclick(item)"
+        >
+        </mt-cell>
+      </mt-search>
+    </div>
+</template>
+
+<script>
+    //const url='https://api.douban.com/v2/movie/search?count=5&q='
+    export default {
+        name: "homeheader",
+      data(){
+          return {
+            value:'',
+            list:[]
+          }
+      },
+      methods:{
+          itemclick(index){
+            alert(index)
+          }
+      },
+      watch:{
+        value(val){
+          this.$axios(url+val).then((data)=>
+            this.list=data.data.subjects
+          )
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
